c64cd0030f03bef45f2063e3f3e05b2f906096e5
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-04 16:56:12 -0300

    created test to verify email duplicated in db

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,16 +1,39 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
-
+  beforeEach(async ()=>{
+    await truncate()
+  })
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
-        name: 'João',
-        email: 'jõao@gmail.com',
+        name: 'Maria',
+        email: 'maria@gmail.com',
         password_hash: '123456789',
       });
     expect(response.body).toHaveProperty('id');
   });
+
+  it('not should register user with email duplicated', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'João',
+        email: 'joao@gmail.com',
+        password_hash: '123456789',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'João',
+        email: 'joao@gmail.com',
+        password_hash: '123456789',
+      });
+
+    expect(response.status).toBe(400)
+  })
 });
