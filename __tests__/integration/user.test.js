bb53bef890ae5e888ec9e1e5b095de3cba327a1a
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-05 09:09:10 -0300

    created test to encrypt password user

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,18 +1,30 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
+
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
-  beforeEach(async ()=>{
-    await truncate()
-  })
+  beforeEach(async () => {await truncate();}) // this code is to clean table in each test
+
+  it('should encrypt password when new user created', async () => {
+    const user = await User.create({
+      name: 'Maria',
+      email: 'maria@gmail.com',
+      password: '123456789',
+    });
+    const comparePassword  = await bcrypt.compare('123456789', user.password_hash);
+    expect(comparePassword).toBe(true)
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Maria',
         email: 'maria@gmail.com',
-        password_hash: '123456789',
+        password: '123456789',
       });
     expect(response.body).toHaveProperty('id');
   });
@@ -23,7 +35,7 @@ describe('User', () => {
       .send({
         name: 'João',
         email: 'joao@gmail.com',
-        password_hash: '123456789',
+        password: '123456789',
       });
 
     const response = await request(app)
@@ -31,7 +43,7 @@ describe('User', () => {
       .send({
         name: 'João',
         email: 'joao@gmail.com',
-        password_hash: '123456789',
+        password: '123456789',
       });
 
     expect(response.status).toBe(400)
